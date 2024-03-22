c473360dc60543618382793095f93e33a56148f7
harrysink <113678645+harrysink@users.noreply.github.com>
2024-03-22 16:07:34 +0100

    react-hook-form addition

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,50 +1,68 @@
-import { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useForm } from 'react-hook-form';
+import { DevTool } from '@hookform/devtools';
 import './SignIn.css';
 
+// type FormValues = {
+//     email: String
+//     password: String
+//     password2: String
+// }
+
 function SignIn() {
-    const [username, setUsername] = useState(' ');
-    const [password, setPassword] = useState(' ');
+    const { register, control, handleSubmit, validate, formState } = useForm()
 
-    const handleUsernameChange = (event) => {
-        setUsername(event.target.value);
-    };
-
-    const handlePasswordChange = (event) => {
-        setPassword(event.target.value);
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    };
-
-    function validateForm() {
-        return username.length > 0 && password.length > 0;
+    const onSubmit = (data) => {
+        console.log("Form has been submitted!", data)
     }
+
+    const { errors } = formState
 
     return (
         <div className="signin">
             <div className="signin-div">
                 <h3>Sign in to your account</h3>
                 <div className="form-div">
-                    <form onSubmit={handleSubmit}>
+                    <form onSubmit={handleSubmit(onSubmit)} noValidate>
                         <input
-                            // value={username}
-                            onChange={handleUsernameChange}
                             type="email"
+                            id="email"
+                            {...register("email",{
+                                pattern: {
+                                    value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                                    message: "invalid email format*"
+                                },
+                                required: {
+                                    value: "true",
+                                    message: "this is a required field*"
+                                },
+                                validate: (fieldValue) => !fieldValue.endsWith("baddomain.com") || "this domina is not supported"
+                            })}
                             placeholder="Enter phone number or email"
-                            required
                         />
+                        <p className="errors">{errors.email?.message}</p>
+
                         <input
-                            // value={password}
-                            onChange={handlePasswordChange}
                             type="password"
+                            id="password"
+                            {...register("password", {
+                                required: {
+                                    value: "true",
+                                    message: "this is a required field*"
+                                },
+                                minLength: {
+                                    value: 8,
+                                    message: "password must be at least 8 characters*"
+                                }
+                            })}
                             placeholder="Enter password"
-                            required
                         />
-                        <button type="submit" disabled={!validateForm}>sign in</button>
+                        <p className="errors">{errors.password?.message}</p>
+
+                        <button type="submit">sign in</button>
                         <p>Don't have an account yet? <a href="/signup"><strong>Sign up</strong></a></p>
                     </form>
+                    <DevTool control={control}/>
                 </div>
             </div>
         </div>
@@ -52,3 +70,58 @@ function SignIn() {
 };
 
 export default SignIn;
+
+
+
+// old form management
+
+// function SignIn() {
+//     const [username, setUsername] = useState(' ');
+//     const [password, setPassword] = useState(' ');
+
+//     const handleUsernameChange = (event) => {
+//         setUsername(event.target.value);
+//     };
+
+//     const handlePasswordChange = (event) => {
+//         setPassword(event.target.value);
+//     };
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//     };
+
+//     function validateForm() {
+//         return username.length > 0 && password.length > 0;
+//     }
+
+//     return (
+//         <div className="signin">
+//             <div className="signin-div">
+//                 <h3>Sign in to your account</h3>
+//                 <div className="form-div">
+//                     <form onSubmit={handleSubmit}>
+//                         <input
+//                             // value={username}
+//                             onChange={handleUsernameChange}
+//                             type="email"
+//                             placeholder="Enter phone number or email"
+//                             required
+//                         />
+//                         <input
+//                             // value={password}
+//                             onChange={handlePasswordChange}
+//                             type="password"
+//                             placeholder="Enter password"
+//                             required
+//                         />
+//                         <button type="submit" disabled={!validateForm}>sign in</button>
+//                         <p>Don't have an account yet? <a href="/signup"><strong>Sign up</strong></a></p>
+//                     </form>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// };
+
+// export default SignIn;
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,30 +1,21 @@
-import { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useForm, useWatch } from 'react-hook-form'
+import { DevTool } from '@hookform/devtools'
 import './SignUp.css';
 
+// type FormValues = {
+//     email: String
+//     password: String
+//     password2: String
+// }
+
 function SignUp() {
-    const [username, setUsername] = useState(' ');
-    const [password, setPassword] = useState(' ');
-    const [passwordtwo, setPasswordTwo] = useState(' ');
+    const form = useForm()
+    const { register, control, handleSubmit, watch, validate, formState, } = form
+    const { errors } = formState
 
-    const handleUsernameChange = (event) => {
-        setUsername(event.target.value);
-    };
-
-    const handlePasswordChange = (event) => {
-        setPassword(event.target.value);
-    };
-
-    const handlePasswordChangeTwo = (event) => {
-        setPasswordTwo(event.target.value);
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    };
-
-    function validateForm() {
-        return username.length > 0 && password.length > 0 && passwordtwo.length > 0;
+    const onSubmit = (data) => {
+        console.log("Form has been submitted!", data)
     }
 
     return (
@@ -32,31 +23,60 @@ function SignUp() {
             <div className="signup-div">
                 <h3>Sign up to create your account</h3>
                 <div className="form-div">
-                    <form onSubmit={handleSubmit}>
+                    <form onSubmit={handleSubmit(onSubmit)} noValidate>
                         <input
-                            placeholder="Enter phone number or email"
-                            // value={username}
-                            onChange={handleUsernameChange}
                             type="email"
-                            required
+                            id="email"
+                            {...register("email", {
+                                pattern: {
+                                    value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                                    message: "invalid email format*"
+                                },
+                                required: {
+                                    value: "true",
+                                    message: "this is a required field*"
+                                },
+                                validate: (fieldValue) => !fieldValue.endsWith("baddomain.com") || "this domian is not supported"
+                            })}
+                            placeholder="Enter phone number or email"
                         />
+                        <p className='errors'>{errors.email?.message}</p>
+
                         <input
-                            // value={password}
-                            onChange={handlePasswordChange}
                             type="password"
+                            id="password"
+                            {...register("password", {
+                                required: {
+                                    value: "true",
+                                    message: "this is a required field*"
+                                },
+                                minLength: {
+                                    value: 8,
+                                    message: "password must be at least 8 characters*"
+                                }
+                            })}
                             placeholder="Enter password"
-                            required
                         />
+                        <p className='errors'>{errors.password?.message}</p>
+
                         <input
-                            // value={passwordtwo}
-                            onChange={handlePasswordChangeTwo}
                             type="password"
+                            id="confirmPassword"
+                            {...register("confirmPassword", {
+                                required: {
+                                    value: "true",
+                                    message: "this is a required field*"
+                                },
+                                validate: (fieldValue) => fieldValue === watch("password") || "passwords do not match*"
+                            })}
                             placeholder="Confirm password"
-                            required
                         />
-                        <button type="submit" disabled={!validateForm}>sign up</button>
+                        <p className='errors'>{errors.confirmPassword?.message}</p>
+
+                        <button type="submit">sign up</button>
                         <p>Already have an account? <a href="/signin"><strong>Sign in</strong></a></p>
                     </form>
+                    <DevTool control={control} />
                 </div>
             </div>
         </div>
@@ -64,3 +84,70 @@ function SignUp() {
 };
 
 export default SignUp;
+
+
+
+// old form management
+
+// function SignUp() {
+//     const [username, setUsername] = useState(' ');
+//     const [password, setPassword] = useState(' ');
+//     const [passwordtwo, setPasswordTwo] = useState(' ');
+
+//     const handleUsernameChange = (event) => {
+//         setUsername(event.target.value);
+//     };
+
+//     const handlePasswordChange = (event) => {
+//         setPassword(event.target.value);
+//     };
+
+//     const handlePasswordChangeTwo = (event) => {
+//         setPasswordTwo(event.target.value);
+//     };
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//     };
+
+//     function validateForm() {
+//         return username.length > 0 && password.length > 0 && passwordtwo.length > 0;
+//     }
+
+//     return (
+//         <div className="signup">
+//             <div className="signup-div">
+//                 <h3>Sign up to create your account</h3>
+//                 <div className="form-div">
+//                     <form onSubmit={handleSubmit}>
+//                         <input
+//                             placeholder="Enter phone number or email"
+//                             // value={username}
+//                             onChange={handleUsernameChange}
+//                             type="email"
+//                             required
+//                         />
+//                         <input
+//                             // value={password}
+//                             onChange={handlePasswordChange}
+//                             type="password"
+//                             placeholder="Enter password"
+//                             required
+//                         />
+//                         <input
+//                             // value={passwordtwo}
+//                             onChange={handlePasswordChangeTwo}
+//                             type="password"
+//                             placeholder="Confirm password"
+//                             required
+//                         />
+//                         <button type="submit" disabled={!validateForm}>sign up</button>
+//                         <p>Already have an account? <a href="/signin"><strong>Sign in</strong></a></p>
+//                     </form>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// };
+
+// export default SignUp;
